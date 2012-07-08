a4ad52373ebd66d177d8097fe0a9395f0da00476
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-07-08 14:40:27 +0900

    master
    v.1
    d.1
    
    D-------------------------------
    Initial
    
    	new file:   Activity_log.mm
    	new file:   COMPOSITIONS/Composition.mm
    	new file:   ChineseReader3/ChineseReader3.mm
    	new file:   "MusicInstrument/STEP 8 \201u\212y\212\355\203A\203v\203\212\201v.mm"
    	new file:   ShoppingList/Class.mm
    	new file:   ShoppingList/Layouts.mm
    	new file:   ShoppingList/ShoppingList.mm
    	new file:   ShoppingList/ShoppingList_a8823ec96e6b.jpeg
    	new file:   a.bat

---
+++ COMPOSITIONS/Composition.mm
@@ -0,0 +1,47 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<attribute_registry FONT_SIZE="24">
+<attribute_name NAME="Dispatch" VISIBLE="true"/>
+<attribute_name NAME="Version" VISIBLE="true"/>
+</attribute_registry>
+<node CREATED="1341448059156" ID="ID_1451544544" MODIFIED="1341448061656" TEXT="Composition">
+<node CREATED="1341448100437" HGAP="-22" ID="ID_527110073" LINK="../Activity_log.mm" MODIFIED="1341448116125" POSITION="right" TEXT="UP" VSHIFT="-159"/>
+<node CREATED="1341448146156" HGAP="44" ID="ID_1712066975" MODIFIED="1341448165046" POSITION="right" TEXT="LOG" VSHIFT="36">
+<node CREATED="1341448167875" ID="ID_1232946957" MODIFIED="1341448210375" TEXT="S_17 Nostalgique">
+<attribute_layout NAME_WIDTH="116" VALUE_WIDTH="38"/>
+<attribute NAME="Version" VALUE="1"/>
+<attribute NAME="Dispatch" VALUE="2"/>
+<node CREATED="1341449700062" ID="ID_1796393161" MODIFIED="1341450233046" TEXT="Write">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1341449707750" ID="ID_1857784163" MODIFIED="1341450233046" TEXT="Body">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1341449710859" ID="ID_755679615" MODIFIED="1341450233046" TEXT="Version, dispatch">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1341449703031" ID="ID_1132254809" MODIFIED="1341450233046" TEXT="Save">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1341449718703" ID="ID_1507270962" MODIFIED="1341450233046" TEXT="COMMIT">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1341449720656" ID="ID_845394327" MODIFIED="1341450800968" TEXT="Save as png">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1341450898609" ID="ID_884394085" MODIFIED="1341450911375" TEXT="Git add">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1341450803625" ID="ID_164803099" MODIFIED="1341450805718" TEXT="Print out"/>
+</node>
+</node>
+<node CREATED="1341450313093" ID="ID_123694142" MODIFIED="1341450315609" POSITION="left" TEXT="Works">
+<node CREATED="1341448167875" ID="ID_434169330" MODIFIED="1341448210375" TEXT="S_17 Nostalgique">
+<attribute_layout NAME_WIDTH="116" VALUE_WIDTH="38"/>
+<attribute NAME="Version" VALUE="1"/>
+<attribute NAME="Dispatch" VALUE="2"/>
+<node CREATED="1341447983296" ID="ID_397864439" MODIFIED="1341447983296" TEXT="C:\WORKS\WS_compose\Works"/>
+</node>
+</node>
+</node>
+</map>
---
+++ MusicInstrument/STEP 8 �u�y��A�v���v.mm
@@ -0,0 +1,63 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<attribute_registry FONT_SIZE="24">
+<attribute_name NAME="Dispatch" VISIBLE="true"/>
+<attribute_name NAME="Version" VISIBLE="true"/>
+</attribute_registry>
+<node CREATED="1341303491343" ID="ID_786150640" MODIFIED="1341303494875" TEXT="STEP 8 &#x300c;&#x697d;&#x5668;&#x30a2;&#x30d7;&#x30ea;&#x300d;">
+<node CREATED="1341295507812" HGAP="-96" ID="ID_563588786" LINK="../Activity_log.mm" MODIFIED="1341324006515" POSITION="right" TEXT="UP" VSHIFT="69">
+<cloud/>
+</node>
+<node CREATED="1341303634765" HGAP="74" ID="ID_1814953839" MODIFIED="1341303656109" POSITION="right" TEXT="LOG" VSHIFT="-3">
+<node CREATED="1341303693578" ID="ID_362420812" MODIFIED="1341303696703" TEXT="Set up">
+<node CREATED="1341303873718" ID="ID_1863245881" MODIFIED="1341303878265" TEXT="&quot;Domino&quot;">
+<node CREATED="1341303889281" ID="ID_964971390" MODIFIED="1341305032890" TEXT="Download">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1341305023359" ID="ID_1626954391" MODIFIED="1341305975437" TEXT="Create midi files">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1341303701484" ID="ID_706862488" MODIFIED="1341310199484" TEXT="rails new">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1341310210718" ID="ID_178664714" MODIFIED="1341310349953" TEXT="git">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1341310213171" ID="ID_1039497162" MODIFIED="1341310349953" TEXT="init">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1341310217578" ID="ID_1579279732" MODIFIED="1341310349953" TEXT="add">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1341310219937" ID="ID_1536194484" MODIFIED="1341310349953" TEXT="gitk">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+<node CREATED="1341303673328" ID="ID_452796227" MODIFIED="1341312358000" TEXT="master">
+<attribute_layout NAME_WIDTH="121" VALUE_WIDTH="35"/>
+<attribute NAME="Version" VALUE="1"/>
+<attribute NAME="Dispatch" VALUE="1"/>
+<node CREATED="1341310353046" ID="ID_1141266429" MODIFIED="1341312354359" TEXT="COMMIT">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1341303673328" HGAP="25" ID="ID_1640821502" MODIFIED="1341323233187" TEXT="master" VSHIFT="128">
+<attribute_layout NAME_WIDTH="121" VALUE_WIDTH="35"/>
+<attribute NAME="Version" VALUE="1"/>
+<attribute NAME="Dispatch" VALUE="2"/>
+<node CREATED="1341310411859" HGAP="79" ID="ID_1411838253" MODIFIED="1341323236078" TEXT="Layout" VSHIFT="-39">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1341310414453" ID="ID_183121555" MODIFIED="1341312335781" TEXT="main.xml">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1341310414453" ID="ID_1120243271" MODIFIED="1341312335781" TEXT="main.xml (Landscape)">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1341310353046" HGAP="84" ID="ID_1661500928" MODIFIED="1341323237718" TEXT="COMMIT" VSHIFT="-49"/>
+<node CREATED="1341312342812" HGAP="93" ID="ID_1288464814" MODIFIED="1341323240000" TEXT="notes.xml" VSHIFT="-41"/>
+</node>
+</node>
+</node>
+</map>
