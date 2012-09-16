90307a13a1e884a58c5d4fbe2c30d8b6b18046a5
Iwabuchi Ken <iwabuchi.k.2010@gmail.com>
2012-09-16 23:36:25 +0900

    Periodical

---
+++ Science_experiments/Science_experiments.mm
@@ -0,0 +1,11 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1347771163339" ID="ID_1880181015" MODIFIED="1347771166411" TEXT="Science experiments">
+<node CREATED="1341371385218" HGAP="-3" ID="ID_505572937" LINK="../Activity_log.mm" MODIFIED="1347771219427" POSITION="left" TEXT="UP" VSHIFT="-85"/>
+<node CREATED="1347771227804" ID="ID_845622099" MODIFIED="1347771229370" POSITION="right" TEXT="LOG">
+<node CREATED="1347771245900" ID="ID_1972680887" MODIFIED="1347771248171" TEXT="E=5">
+<node CREATED="1347771276652" ID="ID_81093701" MODIFIED="1347771288587" TEXT="&#x6d88;&#x77f3;&#x7070;&#xff0b;&#x91cd;&#x66f9;"/>
+</node>
+</node>
+</node>
+</map>
