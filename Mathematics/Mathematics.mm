63501730d53bb41d97c1d844e387861212aecd64
Iwabuchi Ken <iwabuchi.k.2010@gmail.com>
2013-02-27 04:40:47 +0900

    W

--- Mathematics/Mathematics.mm
+++ Mathematics/Mathematics.mm
@@ -4,8 +4,9 @@
 <node CREATED="1358328751533" ID="ID_1417157628" LINK="../Activity_log.mm" MODIFIED="1358328757288" POSITION="left" TEXT="UP"/>
 <node CREATED="1358261952606" ID="ID_1188143904" MODIFIED="1358297625573" POSITION="right" TEXT="LOG">
 <node CREATED="1358328822959" ID="ID_380201056" MODIFIED="1358328830561" TEXT="D-1">
-<node CREATED="1358328831167" ID="ID_297083155" LINK="/home/iwabuchiken/ws/Mathematics/head.pdf" MODIFIED="1358328956899" TEXT="Set theory">
+<node CREATED="1358328831167" ID="ID_297083155" LINK="/home/iwabuchiken/ws/Mathematics/head.pdf" MODIFIED="1361854044552" TEXT="Set theory">
 <node CREATED="1358328958479" ID="ID_1453128623" LINK="https://www.google.co.jp/search?q=stanford+free+course+online&amp;aq=1&amp;oq=stanford+free+course&amp;aqs=chrome.2.0j57j0l2.8744&amp;sugexp=chrome,mod=0&amp;sourceid=chrome&amp;client=ubuntu&amp;channel=cs&amp;ie=UTF-8#hl=ja&amp;client=ubuntu&amp;hs=UZk&amp;tbo=d&amp;channel=cs&amp;sclient=psy-ab&amp;q=free+course+online+set+theory&amp;oq=free+course+online+set+theory&amp;gs_l=serp.3...105970.109267.0.109509.13.13.0.0.0.0.398.1663.8j4j0j1.13.0...0.0...1c.1.-_IcKoGUJmU&amp;pbx=1&amp;bav=on.2,or.r_gc.r_pw.r_qf.&amp;bvm=bv.41018144,d.dGI&amp;fp=55164de4e9dfad14&amp;biw=1535&amp;bih=1088" MODIFIED="1358328982299" TEXT="=&gt; REF"/>
+<node CREATED="1361855589536" ID="ID_1618185805" LINK="http://math.boisestate.edu/~holmes/holmes/head.pdf" MODIFIED="1361855594474" TEXT="Website"/>
 </node>
 <node CREATED="1358689128933" ID="ID_1358764691" MODIFIED="1358689131063" TEXT="s-1">
 <node CREATED="1358330990177" ID="ID_799089742" MODIFIED="1358689118508" TEXT="&quot;A natural way to specify a set is to take the collection of all objects with&quot;">
@@ -32,6 +33,12 @@
 <node CREATED="1358328822959" ID="ID_1188831463" MODIFIED="1358328825058" TEXT="D-"/>
 <node CREATED="1358328822959" ID="ID_1806565035" MODIFIED="1358328825058" TEXT="D-"/>
 <node CREATED="1358328822959" ID="ID_584656293" MODIFIED="1358328825058" TEXT="D-"/>
+</node>
+<node CREATED="1361855710464" ID="ID_60678323" MODIFIED="1361855717467" POSITION="left" TEXT="Areas">
+<node CREATED="1361855718815" ID="ID_935453351" MODIFIED="1361855725647" TEXT="Set Theory"/>
+<node CREATED="1361855726086" ID="ID_1472965517" MODIFIED="1361855728133" TEXT="Integral">
+<node CREATED="1361855741423" ID="ID_8197151" LINK="http://www.mathtutor.ac.uk/integration/integrationassummation/text" MODIFIED="1361855745433" TEXT="Resource"/>
+</node>
 </node>
 </node>
 </map>
