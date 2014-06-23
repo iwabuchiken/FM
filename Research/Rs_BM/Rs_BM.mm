1ecfe70d5b8a3d7d8ae9c425ff8d87222a6dbb19
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-06-23 23:34:41 +0900

    W

--- Research/Rs_BM/Rs_BM.mm
+++ Research/Rs_BM/Rs_BM.mm
@@ -1,11 +1,17 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1353658279906" ID="ID_936318891" MODIFIED="1403445680305" TEXT="Rs_BM">
+<node CREATED="1403487357774" FOLDED="true" ID="ID_1774799286" MODIFIED="1403487369872" POSITION="left" TEXT="Legacies">
+<node CREATED="1341371385218" HGAP="29" ID="ID_505572937" LINK="../Activity_log.mm" MODIFIED="1403487367138" TEXT="UP" VSHIFT="-23"/>
+<node CREATED="1365290703288" ID="ID_1697995904" LINK="../Diary/Diary.mm" MODIFIED="1403487367142" TEXT="Diary"/>
+<node CREATED="1376367601644" ID="ID_430421308" LINK="C:\WORKS\WS\Researches" MODIFIED="1403487367146" TEXT="Folder"/>
+<node CREATED="1381618702830" ID="ID_1353973406" LINK="../Activities_2/Activities_2.mm" MODIFIED="1403487367150" TEXT="Acrtivities_2"/>
+</node>
 <node CREATED="1365290681947" HGAP="36" ID="ID_1617753205" MODIFIED="1365290696488" POSITION="left" TEXT="Link" VSHIFT="-40">
-<node CREATED="1341371385218" HGAP="29" ID="ID_505572937" LINK="../Activity_log.mm" MODIFIED="1365290701272" TEXT="UP" VSHIFT="-23"/>
-<node CREATED="1365290703288" ID="ID_1697995904" LINK="../Diary/Diary.mm" MODIFIED="1365290713093" TEXT="Diary"/>
-<node CREATED="1376367601644" ID="ID_430421308" LINK="C:\WORKS\WS\Researches" MODIFIED="1376367608078" TEXT="Folder"/>
-<node CREATED="1381618702830" ID="ID_1353973406" LINK="../Activities_2/Activities_2.mm" MODIFIED="1381618732923" TEXT="Acrtivities_2"/>
+<node CREATED="1403487300173" ID="ID_401996710" MODIFIED="1403487302921" TEXT="Paper">
+<node CREATED="1403487303389" ID="ID_1817779134" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit" MODIFIED="1403487307106" TEXT="part 1"/>
+<node CREATED="1403487308396" ID="ID_758834264" LINK="https://docs.google.com/document/d/1iYpnmfI9gCG3m54mCu6FRZ9BJF20A8ZmR8ZQthq59Pw/edit" MODIFIED="1403487315096" TEXT="part 2"/>
+</node>
 </node>
 <node BACKGROUND_COLOR="#0033ff" COLOR="#ffffff" CREATED="1350079757054" ID="ID_1031345397" MODIFIED="1403444780847" POSITION="right" TEXT="N@">
 <cloud/>
@@ -19,22 +25,111 @@
 <node CREATED="1403444920325" ID="ID_794999770" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.yii8133rn7aa" MODIFIED="1403445712056" POSITION="right" TEXT="Vertebrate"/>
 <node CREATED="1403444871268" ID="ID_1197639843" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.v001wu1rqput" MODIFIED="1403445714223" POSITION="right" TEXT="What is BM?">
 <icon BUILTIN="full-1"/>
-<node CREATED="1403444947606" ID="ID_1941345440" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.wab9xi65q719" MODIFIED="1403444953864" TEXT="biogenic"/>
-<node CREATED="1403444998100" ID="ID_20683076" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.17sxc6vukho3" MODIFIED="1403445004568" TEXT="fibroin"/>
+<node CREATED="1403444998100" ID="ID_20683076" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.17sxc6vukho3" MODIFIED="1403487908629" TEXT="fibroin">
+<node CREATED="1403487753726" ID="ID_701850168" MODIFIED="1403487891568">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/US07163736-20070116-C00001_20%25.png" />
+  </body>
+</html>
+</richcontent>
 </node>
-<node CREATED="1403445013459" ID="ID_1215617272" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.id4v851xb426" MODIFIED="1403445019515" POSITION="right" TEXT="clams">
-<node CREATED="1403445040390" ID="ID_1004371801" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.acsk2c7pmhkm" MODIFIED="1403445045528" TEXT="mantle"/>
-<node CREATED="1403445055124" ID="ID_1547043924" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.fg17ehcicwph" MODIFIED="1403445060082" TEXT="dorsal"/>
-<node CREATED="1403445066900" ID="ID_1488228328" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.r7j09cxf6nxs" MODIFIED="1403445071882" TEXT="gill"/>
-<node CREATED="1403445083397" ID="ID_420757713" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.tqd9wf6maknw" MODIFIED="1403445089389" TEXT="conchology"/>
+</node>
+</node>
+<node CREATED="1403487985933" ID="ID_955811050" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.id4v851xb426" MODIFIED="1403487996238" POSITION="right" TEXT="clams">
+<node CREATED="1403486787407" ID="ID_849155570" MODIFIED="1403487991171">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="../../../../Downloads/Clams_041913-123x83.jpg" />
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1403445040390" ID="ID_1004371801" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.acsk2c7pmhkm" MODIFIED="1403487564414" TEXT="mantle">
+<node CREATED="1403509066207" ID="ID_1673077503" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.kwpr8c1gnq1n" MODIFIED="1403509084588" TEXT="epidermis S6.Q5">
+<node CREATED="1403509309327" ID="ID_1302979391" MODIFIED="1403509315926">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/skin.jpg" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1403512717264" ID="ID_1702813891" MODIFIED="1403512719132" TEXT="structure">
+<node CREATED="1403512474800" ID="ID_855427340" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.viir9jedotsu" MODIFIED="1403512492159" TEXT="extrapallial S7.Q9"/>
+<node CREATED="1403512276574" ID="ID_399477143" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.ad297xoq4if" MODIFIED="1403512721940" TEXT="epithelium S7.Q8 &#x4e0a;&#x76ae;&#x7d30;&#x80de;"/>
+<node CREATED="1403512163760" ID="ID_1564778110" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.47190wa3k9ky" MODIFIED="1403512723669" TEXT="periostracum S7.Q4"/>
+<node CREATED="1403512114784" ID="ID_1314570090" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.w4pcvl9oyk3p" MODIFIED="1403512725862" TEXT="tissue S7.Q1">
+<node CREATED="1403512136896" ID="ID_503323730" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.grqmghew02ge" MODIFIED="1403512149821" TEXT="organ S7.Q2"/>
+</node>
+<node CREATED="1403512068578" ID="ID_615796983" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.83jaq3h1oubo" MODIFIED="1403512727479" TEXT="germ cell S6.Q9">
+<node CREATED="1403512089663" ID="ID_1241598015" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.t8br5ke07yy3" MODIFIED="1403512105308" TEXT="Gametes S6.Q10"/>
+</node>
+<node CREATED="1403512554592" ID="ID_1071367413" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.lmfu7ecy5poi" MODIFIED="1403512575261" TEXT="the organic constituent in the mollusc shell S7.Q11">
+<node CREATED="1403512584416" ID="ID_1306546256" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.6dvzmf9f5f9b" MODIFIED="1403512598031" TEXT="chitin S7.Q12">
+<node CREATED="1403512613311" ID="ID_314803579" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.tfghytg4g8n8" MODIFIED="1403512629966">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      STRUCTURE of chitin S7.T.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1403512501248" ID="ID_398933040" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.1jyhhh4fpcn9" MODIFIED="1403512749028" TEXT="matrix S7.Q10"/>
+</node>
+</node>
+</node>
+<node CREATED="1403445055124" ID="ID_1547043924" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.fg17ehcicwph" MODIFIED="1403487564410" TEXT="dorsal"/>
+<node CREATED="1403445066900" ID="ID_1488228328" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.r7j09cxf6nxs" MODIFIED="1403487564406" TEXT="gill"/>
+<node CREATED="1403445083397" ID="ID_420757713" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.tqd9wf6maknw" MODIFIED="1403487564402" TEXT="conchology"/>
 </node>
 <node CREATED="1403445123332" ID="ID_521053117" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.vs7tjunsgjya" MODIFIED="1403445129291" POSITION="right" TEXT="polysaccharides">
 <node CREATED="1403445134451" ID="ID_1062500608" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.dd9np9nz1dx1" MODIFIED="1403445142301" TEXT="polymeric"/>
-<node CREATED="1403445152532" ID="ID_1602639127" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.1ubnheblihli" MODIFIED="1403445158273" TEXT="carbohydrate"/>
+<node CREATED="1403487957261" ID="ID_164018817" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.1ubnheblihli" MODIFIED="1403487961554" TEXT="carbohydrate">
+<node CREATED="1403487580430" ID="ID_520642620" MODIFIED="1403487972563">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="images/200px-Lactose.svg.png" />
+    </p>
+  </body>
+</html></richcontent>
 </node>
+<node CREATED="1403508939183" ID="ID_1678300586" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.61xneo3b7kpl" MODIFIED="1403508981407" TEXT="biomolecule"/>
+<node CREATED="1403509014223" ID="ID_1989881677" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.h7z04hqr4rmh" MODIFIED="1403509021613" TEXT="hydrate"/>
+<node CREATED="1403444947606" ID="ID_1941345440" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.wab9xi65q719" MODIFIED="1403444953864" TEXT="biogenic"/>
+</node>
+</node>
+<node CREATED="1403508996127" ID="ID_398505979" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.q32002171zi5" MODIFIED="1403509004317" POSITION="right" TEXT="empirical formula"/>
 <node CREATED="1403445178789" ID="ID_682939642" MODIFIED="1403445180480" POSITION="left" TEXT="NEXT">
-<node CREATED="1403445171508" ID="ID_807541675" MODIFIED="1403445183385" TEXT="S5.Q7">
+<node CREATED="1403445171508" ID="ID_807541675" MODIFIED="1403509402503" TEXT="S5.Q7">
+<icon BUILTIN="button_ok"/>
 <node CREATED="1403445198713" MODIFIED="1403445198713" TEXT="22/06/2014 22:53:18"/>
+</node>
+<node CREATED="1403509394704" ID="ID_1162333161" MODIFIED="1403509399324" TEXT="S6.Q6">
+<node CREATED="1403509415635" MODIFIED="1403509415635" TEXT="23/06/2014 16:43:34"/>
+</node>
+<node CREATED="1403512649503" ID="ID_1495517078" MODIFIED="1403512650721" TEXT="S8.Q1">
+<node CREATED="1403512672638" MODIFIED="1403512672638" TEXT="23/06/2014 17:37:50"/>
 </node>
 </node>
 </node>
