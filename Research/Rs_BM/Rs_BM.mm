3a779bfb87b712d4ee5d1e5075a7a866cffa02ee
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-06-24 21:40:45 +0900

    W

--- Research/Rs_BM/Rs_BM.mm
+++ Research/Rs_BM/Rs_BM.mm
@@ -7,11 +7,18 @@
 <node CREATED="1376367601644" ID="ID_430421308" LINK="C:\WORKS\WS\Researches" MODIFIED="1403487367146" TEXT="Folder"/>
 <node CREATED="1381618702830" ID="ID_1353973406" LINK="../Activities_2/Activities_2.mm" MODIFIED="1403487367150" TEXT="Acrtivities_2"/>
 </node>
-<node CREATED="1365290681947" HGAP="36" ID="ID_1617753205" MODIFIED="1365290696488" POSITION="left" TEXT="Link" VSHIFT="-40">
-<node CREATED="1403487300173" ID="ID_401996710" MODIFIED="1403487302921" TEXT="Paper">
+<node CREATED="1365290681947" HGAP="36" ID="ID_1617753205" MODIFIED="1403587176113" POSITION="left" TEXT="Link" VSHIFT="-40">
+<node CREATED="1403487300173" ID="ID_401996710" MODIFIED="1403587177200" TEXT="Paper">
 <node CREATED="1403487303389" ID="ID_1817779134" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit" MODIFIED="1403487307106" TEXT="part 1"/>
 <node CREATED="1403487308396" ID="ID_758834264" LINK="https://docs.google.com/document/d/1iYpnmfI9gCG3m54mCu6FRZ9BJF20A8ZmR8ZQthq59Pw/edit" MODIFIED="1403487315096" TEXT="part 2"/>
 </node>
+<node CREATED="1403587178663" ID="ID_932058106" MODIFIED="1403588629697" TEXT="java -cp &quot;C:/WORKS/WS/Eclipse_Kepler/ImageResizer/bin;C:/WORKS/WS/Eclipse_Kepler/ImageResizer/libs/*&quot; ir.main.Main  -size 20,20 -src C:\WORKS\WS\Eclipse_Kepler\ImageResizer\images\200px-Lactose.svg.png"/>
+<node CREATED="1403587178663" ID="ID_1562436910" MODIFIED="1403589140816" TEXT="java -cp &quot;C:/WORKS/WS/Eclipse_Kepler/ImageResizer/bin;C:/WORKS/WS/Eclipse_Kepler/ImageResizer/libs/*&quot; ir.main.Main  -size 20,20 -src C:\WORKS\WS\FM\Research\Rs_BM\images"/>
+<node CREATED="1403587221556" ID="ID_623795445" LINK="C:\WORKS\WS\Eclipse_Kepler\ImageResizer\images" MODIFIED="1403587224288" TEXT="C:\WORKS\WS\Eclipse_Kepler\ImageResizer\images"/>
+<node CREATED="1403587178663" ID="ID_959594510" MODIFIED="1403587865184" TEXT="java -cp &quot;bin;libs/*&quot; ir.main.Main -size 20,20 -src C:\WORKS\Downloads\"/>
+<node CREATED="1403588458741" ID="ID_941244754" MODIFIED="1403588464609" TEXT="copy C:\WORKS\Downloads\"/>
+<node CREATED="1403588500261" ID="ID_1669465809" MODIFIED="1403588501665" TEXT="C:\WORKS\WS\FM\Research\Rs_BM\images"/>
+<node CREATED="1403588505304" ID="ID_1319180117" MODIFIED="1403588510258" TEXT="pushd C:\WORKS\WS\FM\Research\Rs_BM\images"/>
 </node>
 <node BACKGROUND_COLOR="#0033ff" COLOR="#ffffff" CREATED="1350079757054" ID="ID_1031345397" MODIFIED="1403444780847" POSITION="right" TEXT="N@">
 <cloud/>
@@ -34,24 +41,11 @@
   <body>
     <img src="images/US07163736-20070116-C00001_20%25.png" />
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
 <node CREATED="1403487985933" ID="ID_955811050" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.id4v851xb426" MODIFIED="1403487996238" POSITION="right" TEXT="clams">
-<node CREATED="1403486787407" ID="ID_849155570" MODIFIED="1403487991171">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      <img src="../../../../Downloads/Clams_041913-123x83.jpg" />
-    </p>
-  </body>
-</html></richcontent>
-</node>
 <node CREATED="1403445040390" ID="ID_1004371801" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.acsk2c7pmhkm" MODIFIED="1403487564414" TEXT="mantle">
 <node CREATED="1403509066207" ID="ID_1673077503" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.kwpr8c1gnq1n" MODIFIED="1403509084588" TEXT="epidermis S6.Q5">
 <node CREATED="1403509309327" ID="ID_1302979391" MODIFIED="1403509315926">
@@ -62,8 +56,7 @@
   <body>
     <img src="images/skin.jpg" />
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 <node CREATED="1403512717264" ID="ID_1702813891" MODIFIED="1403512719132" TEXT="structure">
@@ -98,11 +91,26 @@
 <node CREATED="1403445055124" ID="ID_1547043924" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.fg17ehcicwph" MODIFIED="1403487564410" TEXT="dorsal"/>
 <node CREATED="1403445066900" ID="ID_1488228328" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.r7j09cxf6nxs" MODIFIED="1403487564406" TEXT="gill"/>
 <node CREATED="1403445083397" ID="ID_420757713" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.tqd9wf6maknw" MODIFIED="1403487564402" TEXT="conchology"/>
+<node CREATED="1403593411574" ID="ID_377518496" MODIFIED="1403593414675" TEXT="pict">
+<node CREATED="1403593415366" ID="ID_721122566" MODIFIED="1403593427600">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/Clams_041913-123x83.jpg" />
+  </body>
+</html>
+</richcontent>
 </node>
-<node CREATED="1403445123332" ID="ID_521053117" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.vs7tjunsgjya" MODIFIED="1403445129291" POSITION="right" TEXT="polysaccharides">
+</node>
+</node>
+<node CREATED="1403587449844" ID="ID_1522574609" MODIFIED="1403587452712" POSITION="right" TEXT="Chemistry">
+<node CREATED="1403445123332" ID="ID_521053117" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.vs7tjunsgjya" MODIFIED="1403445129291" TEXT="polysaccharides">
 <node CREATED="1403445134451" ID="ID_1062500608" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.dd9np9nz1dx1" MODIFIED="1403445142301" TEXT="polymeric"/>
-<node CREATED="1403487957261" ID="ID_164018817" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.1ubnheblihli" MODIFIED="1403487961554" TEXT="carbohydrate">
-<node CREATED="1403487580430" ID="ID_520642620" MODIFIED="1403487972563">
+<node CREATED="1403487957261" ID="ID_164018817" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.1ubnheblihli" MODIFIED="1403590820482" TEXT="carbohydrate S6.Q1">
+<node CREATED="1403593375366" FOLDED="true" ID="ID_555550573" MODIFIED="1403593380008" TEXT="pict">
+<node CREATED="1403487580430" ID="ID_520642620" LINK="http://en.wikipedia.org/wiki/File:Lactose.svg" MODIFIED="1403590754659">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -110,26 +118,201 @@
   <body>
     <p>
       <img src="images/200px-Lactose.svg.png" />
+      Lactose
     </p>
   </body>
 </html></richcontent>
+</node>
 </node>
 <node CREATED="1403508939183" ID="ID_1678300586" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.61xneo3b7kpl" MODIFIED="1403508981407" TEXT="biomolecule"/>
 <node CREATED="1403509014223" ID="ID_1989881677" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.h7z04hqr4rmh" MODIFIED="1403509021613" TEXT="hydrate"/>
 <node CREATED="1403444947606" ID="ID_1941345440" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.wab9xi65q719" MODIFIED="1403444953864" TEXT="biogenic"/>
 </node>
 </node>
-<node CREATED="1403508996127" ID="ID_398505979" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.q32002171zi5" MODIFIED="1403509004317" POSITION="right" TEXT="empirical formula"/>
+<node CREATED="1403587454725" ID="ID_1011126086" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.1fyjqd41l342" MODIFIED="1403587512726" TEXT="functional group S8.Q1"/>
+<node CREATED="1403508996127" ID="ID_398505979" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.q32002171zi5" MODIFIED="1403509004317" TEXT="empirical formula"/>
+<node CREATED="1403587487188" ID="ID_1825424334" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.5zgxdupnbf7q" MODIFIED="1403588139184" TEXT="glycoproteins S13.Q1">
+<node CREATED="1403588873525" FOLDED="true" ID="ID_1000521577" MODIFIED="1403588880854" TEXT="pict">
+<node CREATED="1403588079204" ID="ID_1297372653" MODIFIED="1403588174659">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="../../../../Downloads/12Nlinkglycoprot.gif" />
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1403587520436" ID="ID_1996867291" LINK="https://docs.google.com/document/d/1iOCBz412QcYU64F6HBmuTZTctjtDnfHiuTKKp1rcNiQ/edit#heading=h.pzmktc6czdlx" MODIFIED="1403587533573" TEXT="polypeptide S13.Q2">
+<node CREATED="1403588864197" FOLDED="true" ID="ID_1819909225" MODIFIED="1403589675009" TEXT="pict">
+<node CREATED="1403588673205" ID="ID_1023437218" MODIFIED="1403588694118">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/tripeptide.png" />
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1403588801988" ID="ID_128299856" MODIFIED="1403588859599" TEXT="peptide S13.Q3">
+<node CREATED="1403588890709" FOLDED="true" ID="ID_1636434223" MODIFIED="1403593368600" TEXT="pict">
+<node CREATED="1403589248773" ID="ID_1475779946" MODIFIED="1403589254208">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/peptide_bond01_20140624_145347_size-50x50.jpg" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1403589704821" ID="ID_1605127017" MODIFIED="1403589718817" TEXT="amino acid S13.Q4">
+<node CREATED="1403589842389" FOLDED="true" ID="ID_1850903904" MODIFIED="1403593367096" TEXT="pict">
+<node CREATED="1403589844468" ID="ID_632661091" LINK="http://en.wikipedia.org/wiki/Amino_acid#mediaviewer/File:AminoAcidball.svg" MODIFIED="1403589869975">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/204px-AminoAcidball.svg.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1403589894885" ID="ID_1412106788" MODIFIED="1403589905746" TEXT="carboxylic acid S13.Q5">
+<node CREATED="1403589907013" FOLDED="true" ID="ID_824538921" MODIFIED="1403593366088" TEXT="pict">
+<node CREATED="1403589958197" ID="ID_131072535" LINK="http://en.wikipedia.org/wiki/Carboxylic_acid#mediaviewer/File:Carboxylic-acid.svg" MODIFIED="1403589969573">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/181px-Carboxylic-acid.svg.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1403590026646" ID="ID_726141223" MODIFIED="1403590036769" TEXT="oligosaccharide S13.Q6">
+<node CREATED="1403590038148" FOLDED="true" ID="ID_1077770702" MODIFIED="1403593361893" TEXT="pict">
+<node CREATED="1403590040694" ID="ID_96868590" LINK="http://en.wikipedia.org/wiki/Oligosaccharide_nomenclature#mediaviewer/File:Sucrose.svg" MODIFIED="1403590241683">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="images/220px-Sucrose.svg.png" />
+      sucrose
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1403590870598" ID="ID_721116795" MODIFIED="1403590879859" TEXT="protein S13.Q7">
+<node CREATED="1403591015061" FOLDED="true" ID="ID_1459314617" MODIFIED="1403593357286" TEXT="pict">
+<node CREATED="1403591016933" ID="ID_615574285" LINK="http://www.milkfacts.info/Milk%20Composition/Protein.htm" MODIFIED="1403591036752">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/protein.JPG" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1403591121798" ID="ID_267826373" MODIFIED="1403591570338" TEXT="macromolecules S13.Q8">
+<node CREATED="1403591015061" FOLDED="true" ID="ID_1833966668" MODIFIED="1403593356183" TEXT="pict">
+<node CREATED="1403591156293" ID="ID_624926307" LINK="http://kristaweng15.blogspot.jp/2011/11/macromolecule-structure.html" MODIFIED="1403591530351">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/DehSyn1.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1403591620806" ID="ID_1541202831" MODIFIED="1403591630882" TEXT="quinone S15.Q1">
+<node CREATED="1403591015061" FOLDED="true" ID="ID_1630289436" MODIFIED="1403593353906" TEXT="pict">
+<node CREATED="1403591637702" ID="ID_712724952" LINK="http://archives.library.illinois.edu/erec/University%20Archives/1505050/Organic/Alcohols/Chapter%206/sec6-14/6-14.htm" MODIFIED="1403592648088">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/fig6-23.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1403592453510" ID="ID_186990656" MODIFIED="1403592463666" TEXT="conjugation S15.Q2">
+<node CREATED="1403591015061" FOLDED="true" ID="ID_1559750709" MODIFIED="1403593358998" TEXT="pict">
+<node CREATED="1403591637702" ID="ID_1368475638" LINK="http://en.wikipedia.org/wiki/File:Benzene_Orbitals.svg" MODIFIED="1403592640696">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/200px-Benzene_Orbitals.svg.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1403592675894" ID="ID_1583467244" MODIFIED="1403592684291" TEXT="benzene S15.Q3">
+<node CREATED="1403591015061" FOLDED="true" ID="ID_651943050" MODIFIED="1403593359861" TEXT="pict">
+<node CREATED="1403591637702" ID="ID_1279119233" LINK="http://www.meritnation.com/ask-answer/question/please-give-me-a-brief-description-of-benzene-and-benzine-wi/science/3092370" MODIFIED="1403593244008">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/3432.16.benzene-ring_20140624_160002_size-50x50.jpg" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
 <node CREATED="1403445178789" ID="ID_682939642" MODIFIED="1403445180480" POSITION="left" TEXT="NEXT">
 <node CREATED="1403445171508" ID="ID_807541675" MODIFIED="1403509402503" TEXT="S5.Q7">
 <icon BUILTIN="button_ok"/>
 <node CREATED="1403445198713" MODIFIED="1403445198713" TEXT="22/06/2014 22:53:18"/>
 </node>
-<node CREATED="1403509394704" ID="ID_1162333161" MODIFIED="1403509399324" TEXT="S6.Q6">
+<node CREATED="1403509394704" ID="ID_1162333161" MODIFIED="1403593317412" TEXT="S6.Q6">
+<icon BUILTIN="button_ok"/>
 <node CREATED="1403509415635" MODIFIED="1403509415635" TEXT="23/06/2014 16:43:34"/>
 </node>
-<node CREATED="1403512649503" ID="ID_1495517078" MODIFIED="1403512650721" TEXT="S8.Q1">
+<node CREATED="1403512649503" ID="ID_1495517078" MODIFIED="1403593317413" TEXT="S8.Q1">
+<icon BUILTIN="button_ok"/>
 <node CREATED="1403512672638" MODIFIED="1403512672638" TEXT="23/06/2014 17:37:50"/>
+</node>
+<node CREATED="1403593305254" ID="ID_1068383341" MODIFIED="1403593305682" TEXT="S18.Q1">
+<node CREATED="1403593315241" MODIFIED="1403593315241" TEXT="24/06/2014 16:01:56"/>
 </node>
 </node>
 </node>
